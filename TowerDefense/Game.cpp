c8b2cd62d7279aeacbcdf76691177c73858340b7
Bleonov98 <koslok2013@gmail.com>
2024-02-21 23:44:44 +0200

    .

--- TowerDefense/Game.cpp
+++ TowerDefense/Game.cpp
@@ -45,6 +45,26 @@ void Game::Render(float dt)
 {
 }
 
+void Game::DrawObject(GameObject* obj)
+{
+	ResourceManager::GetShader("modelShader").Use();
+	ResourceManager::GetShader("modelShader").SetMatrix4("projection", projection);
+	ResourceManager::GetShader("modelShader").SetMatrix4("view", view);
+
+	ResourceManager::GetShader("modelShader").SetVector3f("objectColor", obj->GetColor());
+	// modelShader.SetVector3f("lightColor", light.GetColor());
+	// modelShader.SetVector3f("lightPos", light.GetPosition());
+	// modelShader.SetVector3f("viewPos", camera.GetCameraPosition());
+
+	glm::mat4 model = glm::mat4(1.0f);
+
+	model = glm::translate(model, obj->GetPosition());
+	model = glm::scale(model, obj->GetSize());
+
+	ResourceManager::GetShader("modelShader").SetMatrix4("model", model);
+	obj->DrawObject();
+}
+
 void Game::DrawMenu()
 {
 	text->RenderText("MENU", glm::vec2(this->width / 2.0f - 65.0f, this->height / 2.0f - 116.0f), 1.75f, glm::vec3(0.75f));
--- TowerDefense/GameObject.h
+++ TowerDefense/GameObject.h
@@ -15,10 +15,12 @@ public:
 
 	glm::vec3 GetPosition() { return position; }
 	glm::vec3 GetSize() { return size; }
+	glm::vec3 GetColor() { return color; }
 	float GetAngle() { return angle; }
 
 	void SetPosition(glm::vec3 pos) { this->position = pos; }
 	void SetSize(glm::vec3 size) { this->size = size; }
+	void SetColor(glm::vec3 color) { this->color = color; }
 	void SetAngle(float angle) { this->angle = angle; }
 
 	void SetModel(Model model) { this->model = model; }
@@ -38,7 +40,7 @@ protected:
 
 	bool deleted = false;
 	float angle;
-	glm::vec3 position, size;
+	glm::vec3 position, size, color = glm::vec3(1.0f);
 
 };
 #endif // !GAMEOBJECT_H
